a77138084521a92488d49d13552b2681272b7019
dohaianh <dha190320022@gmail.com>
2022-10-20 06:16:13 +0700

    deploy app

--- src/main/java/com/blog/MyBlogApplication.java
+++ src/main/java/com/blog/MyBlogApplication.java
@@ -4,7 +4,6 @@ import com.blog.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
